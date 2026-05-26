d62a958be62d407828f6d7955fd55d78a1fd014f
Ryan Capouellez <rjc8237@nyu.edu>
2026-05-26 13:42:02 -0400

    Added less refined feature aligned field and post optimization

--- src/app/parameterize_aligned.cpp
+++ src/app/parameterize_aligned.cpp
@@ -16,6 +16,137 @@ using namespace Penner::Optimization;
 using namespace Penner::Holonomy;
 using namespace Penner::Feature;
 
+#include "ExtremeOpt.h"
+#include "MeshCutter.h"
+#include "main_helper.h"
+
+
+SymDir::Parameters read_parameters(const nlohmann::json& config)
+{
+    SymDir::Parameters param;
+    param.max_iters = config["max_iters"]; // iterations
+    param.max_time = config["max_time"]; // time in seconds
+    param.smooth_only_iters = config["smooth_only_iters"];
+    param.E_target = config["E_target"]; // Energy target
+    param.ls_iters = config["ls_iters"]; // param for linesearch in smoothing operation
+    param.do_newton = config["do_newton"]; // do newton/gd steps for smoothing operation
+    param.local_smooth = config["local_smooth"];
+    param.global_smooth = config["global_smooth"];
+    param.elen_alpha = config["elen_alpha"];
+    param.do_projection = config["do_projection"];
+    param.with_cons = config["with_cons"];
+    param.Lp = config["Lp"];
+    param.save_meshes = config["save_meshes"];
+    param.do_feature_alignment = config["do_feature_alignment"]; // align feature edges
+    param.symdir_weight = config["symdir_weight"];
+    param.alignment_weight = config["alignment_weight"];
+    param.degenerate_weight = config["degenerate_weight"];
+    param.fix_misaligned = config["fix_misaligned"];
+    param.use_rref = config["use_rref"];
+    // param.solver_type = config["solver_type"];
+    param.cg_rel_err = config["cg_rel_err"];
+    param.cg_iters = config["cg_iters"];
+    
+    param.percentages = config["percentages"].get<std::vector<double>>();
+    param.percentage_target = config["percentage_target"];
+    param.percentage_target_value = config["percentage_target_value"];
+    param.save_percentages_meshes = config["save_percentages_meshes"];
+
+    param.E_abs_err = config["E_abs_err"];
+    param.E_rel_err = config["E_rel_err"];
+    param.diff_err = config["diff_err"];
+    param.grad_abs_err = config["grad_abs_err"];
+    param.grad_rel_err = config["grad_rel_err"];
+    
+    param.precompute_seamless = config["precompute_seamless"];
+    param.projected_newton = config["projected_newton"];
+    param.soft_max = config["soft_max"];
+    param.t = config["t"];
+    param.precompute_seamless = config["precompute_seamless"];
+    
+    param.percentage_target_converge = config["percentage_target_converge"];
+    param.max_grad_abs_converge = config["max_grad_abs_converge"];
+    param.max_grad_rel_converge = config["max_grad_rel_converge"];
+    param.energy_diff_converge = config["energy_diff_converge"];
+    param.use_worst_n_energy_in_ls = config["use_worst_n_energy_in_ls"];
+    param.E_abs_converge = config["E_abs_converge"];
+    param.E_rel_converge = config["E_rel_converge"];
+
+    param.last_screenshot_after_optimization = config["last_screenshot_after_optimization"];
+    param.screenshot_interval = config["screenshot_interval"];
+    param.output_dir_for_screenshots = config["output_dir_for_screenshots"];
+    param.uv_scale_for_screenshots = config["uv_scale_for_screenshots"];
+    param.angle_to_rotate_model_for_screenshots = config["angle_to_rotate_model_for_screenshots"];
+    param.screenshot_during_optimization = config["screenshot_during_optimization"];
+
+    param.degenerate_vertices_preconditioner = config["degenerate_vertices_preconditioner"];
+    param.precond_dim = config["precond_dim"];
+    param.triangle_threshold = config["triangle_threshold"];
+
+    return param;
+}   
+
+Eigen::MatrixXd optimize_aligned_parameterization(
+    const Eigen::MatrixXd& V_init,
+    const Eigen::MatrixXi& F_init,
+    const Eigen::MatrixXd& uv,
+    const Eigen::MatrixXi& F,
+    const Eigen::MatrixXd& PD1,
+    const Eigen::MatrixXd& PD2,
+    const Eigen::MatrixXi& FE_init,
+    const Eigen::MatrixXi& ME,
+    const nlohmann::json& config
+) {
+    SymDir::Parameters param = read_parameters(config);
+
+    igl::Timer timer;
+    double time = 0;
+    timer.start();
+	MeshCutter meshcutter(V_init, uv, F_init, F);
+	auto [V, EE] = meshcutter.cut_mesh();
+    Eigen::MatrixXi FE(0, 0);
+    time = timer.getElapsedTime();
+    if (param.do_feature_alignment)
+    {
+        // Loading the feature edge constraints
+        FE = meshcutter.reindex_feature_edges(FE_init);
+    }
+    double cons_residual = check_constraints(EE, FE, uv, F);
+    spdlog::info("Initial constraints error {}", cons_residual);
+
+    Eigen::MatrixXi new_F;
+    Eigen::MatrixXd new_V, new_uv;
+    SymDir::ExtremeOpt extremeopt(V, F);
+    extremeopt.m_params = param;
+    
+    extremeopt.create_mesh(V, F, uv);
+    extremeopt.set_v_map(F_init, F);
+
+    if (extremeopt.m_params.with_cons)
+    {
+        std::vector<std::vector<int>> EE_e = transform_EE(F, EE);
+        std::vector<std::vector<int>> FE_e;
+        if (extremeopt.m_params.do_feature_alignment) {
+            FE_e = transform_FE(F, FE);
+        }
+        extremeopt.init_constraints(EE_e);
+        extremeopt.EE = EE;
+        extremeopt.FE = FE;
+        extremeopt.ME = ME;
+    }
+
+    //extremeopt.view();
+    extremeopt.PD1 = PD1;
+    extremeopt.PD2 = PD2;
+
+    Eigen::MatrixXi F_opt = F;
+    Eigen::MatrixXd uv_opt;
+    extremeopt.do_optimization_without_log();
+    extremeopt.export_mesh(V, F_opt, uv_opt);
+
+    return uv_opt;
+}
+
 int main(int argc, char* argv[])
 {
     spdlog::set_level(spdlog::level::info);
@@ -25,6 +156,14 @@ int main(int argc, char* argv[])
     std::string mesh = "";
     std::string input_dir = "./";
     std::string output_dir = "./";
+    std::filesystem::path current_dir = std::filesystem::path(__FILE__).parent_path();
+    std::filesystem::path input_json = current_dir / "symdir.json";
+    bool use_uniform_bc = false;
+
+    int full_itr = 50;
+    NewtonParameters alg_params;
+    int max_itr = alg_params.max_itr;
+    spdlog::level::level_enum log_level = spdlog::level::info;
 
     // IO Parameters
     bool use_existing_field = false;
@@ -33,10 +172,16 @@ int main(int argc, char* argv[])
     app.add_option("-i,--input", input_dir, "Input directory")->check(CLI::ExistingDirectory)->required();
     app.add_option("-o,--output", output_dir, "Output directory");
     app.add_flag("--use_existing_field", use_existing_field, "Use precomputed field at the input directory");
+    app.add_flag("--use_uniform_bc", use_uniform_bc, "Use uniform barycentric coordinates");
     app.add_flag("--show_parameterization", show_parameterization, "Show aligned parameterization");
+    app.add_option("--full_itr", full_itr, "Initial iterations of full (potentially unsatisfiable) constraints");
+    app.add_option("--log_level", log_level, "Level of logging")
+        ->transform(CLI::CheckedTransformer(log_level_map, CLI::ignore_case));
+
+    CLI11_PARSE(app, argc, argv);
+    spdlog::set_level(log_level);
 
     // Marked Metric Parameters
-    NewtonParameters alg_params;
     add_newton_parameters(app, alg_params);
     CLI11_PARSE(app, argc, argv);
 
@@ -111,13 +256,60 @@ int main(int argc, char* argv[])
         kappa,
         period_jump,
         marked_metric_params);
+
+    // run iterations of fully optimized method
+    alg_params.max_itr = full_itr;
     aligned_metric_generator.optimize_full(alg_params);
+
+    // run iterations of relaxed optimization
+    alg_params.max_itr = max_itr;
     aligned_metric_generator.optimize_relaxed(alg_params);
-    aligned_metric_generator.parameterize(false);
+
+    aligned_metric_generator.parameterize(false, use_uniform_bc);
     auto [V_r, F_r, uv_r, FT_r, fn_to_f_r, endpoints_r] = aligned_metric_generator.get_parameterization();
     auto [feature_face_edges, misaligned_edges] = aligned_metric_generator.get_refined_features();
     auto feature_edges_r = compute_face_edge_endpoints(feature_face_edges, F_r);
     auto [reference_field_r, theta_r, kappa_r, period_jump_r] = aligned_metric_generator.get_refined_field();
+
+    // Optionally optimize parameterization 
+    bool optimize = true;
+    if (optimize)
+    {
+        // TODO: this is broken for multiple components
+        auto [PD1, PD2] = comb_frame_field(
+            V_r,
+            F_r,
+            uv_r,
+            FT_r,
+            reference_field_r,
+            theta_r,
+            period_jump_r
+        );
+
+        std::ifstream js_in(input_json);
+        nlohmann::json config = nlohmann::json::parse(js_in);
+        config["model"] = mesh;
+        int num_features = feature_edges_r.size();
+        Eigen::MatrixXi FE(num_features, 2);
+        for (int eij = 0; eij < num_features; ++eij)
+        {
+            FE(eij, 0) = feature_edges_r[eij][0];
+            FE(eij, 1) = feature_edges_r[eij][1];
+        }
+
+        // TODO: build ME
+        Eigen::MatrixXi ME(0,0);
+        uv_r = optimize_aligned_parameterization(
+            V_r,
+            F_r,
+            uv_r,
+            FT_r,
+            PD1,
+            PD2,
+            FE,
+            ME,
+            config);
+    }
 
     if (show_parameterization) view_seamless_parameterization(V_r, F_r, uv_r, FT_r, "refined mesh", true);
 
@@ -131,5 +323,7 @@ int main(int argc, char* argv[])
 
     //std::string output_filename = join_path(output_dir, "optimized_corner_coords");
     //write_matrix(opt_corner_coords, output_filename, " ");
+
+
 
 }
